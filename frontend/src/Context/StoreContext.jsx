139d57e25654a3dcd5523d8a8ff62a80a18d528c
Samratavi3 <samratavi94@gmail.com>
2024-08-02 15:38:12 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setcartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://samfood.onrender.com";
   const [token, setToken] = useState("");
   const [foodlist, setFoodlist] = useState([]);
 
